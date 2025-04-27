20258d5dee57e962b00410022f1d38e833cd3f0d
root <root@host.ahservers.net>
2025-04-27 22:53:09 +0000

    Connecting Cartesia tts

--- speech/process_speech.py
+++ speech/process_speech.py
@@ -57,36 +57,49 @@ def transcribe_audio():
 def text_to_speech():
     print("content length header: {}".format(request.content_length))
 
-    from flask import send_file
-    from cartesia import Cartesia
-    
-    client = Cartesia(api_key=os.environ.get("CARTESIA_API_KEY"))
+    if request.content_type == "application/json":
+        from flask import send_file
+        from cartesia import Cartesia
+        
+        client = Cartesia(api_key=os.environ.get("CARTESIA_API_KEY"))
 
-    try:
-        voice_id = os.environ.get("CARTESIA_VOICE_ID")
-        voice = client.voices.get(id=voice_id)
-        print(voice)
-
-        file_path = "output.wav"
-
-        audio_bytes = client.tts.bytes(model_id="sonic-2", transcript="Hello, world!", voice={"mode": "id","id": voice_id}, output_format={"container":"mp3","bit_rate":128000,"sample_rate":44100} )
-        with open(file_path, "wb") as f:
-            for chunk in audio_bytes:
-                f.write(chunk)
-            f.close()
         try:
-            return send_file(file_path, as_attachment=True)
-        except Exception as e:
-            return f"Error sending file: {str(e)}", 500
+            data = request.get_json()
+            data = json.dumps(data, ensure_ascii=False).encode('utf-8').decode('unicode-escape')
 
-    except Exception as e:
-        error_message = str(e)
-        print(f"An error occurred: {error_message}")
-        return Response("There was an error: " + error_message,status=500)
-    
-    return jsonify({
-            'msg': "wrote file ;)"
-            })
+            data  = json.loads(data)
+            #msg = data.body.msg
+            msg =  data['msg']
+
+            voice_id = os.environ.get("CARTESIA_VOICE_ID")
+            voice = client.voices.get(id=voice_id)
+            print(voice)
+
+            file_path = "output.wav"
+
+            audio_bytes = client.tts.bytes(model_id="sonic-2", transcript=msg, voice={"mode": "id","id": voice_id}, output_format={"container":"mp3","bit_rate":128000,"sample_rate":44100} )
+            with open(file_path, "wb") as f:
+                for chunk in audio_bytes:
+                    f.write(chunk)
+                f.close()
+            try:
+                return send_file(file_path, as_attachment=True)
+            except Exception as e:
+                return f"Error sending file: {str(e)}", 500
+
+        except Exception as e:
+            error_message = str(e)
+            print(f"An error occurred: {error_message}")
+            return Response("There was an error: " + error_message,status=500)
+        
+        return jsonify({
+                'msg': "wrote file ;)"
+                })
+    else:
+        return jsonify({
+            'msg': "415 Unsupported Media Type ;)"
+        })
+
 
     #data = request.get_data()
     #return jsonify(data)
